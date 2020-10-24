3ca4099d6b0745075011f94f25f38edef1ee183e
Zaetsfit <54208491+Zaetsfit@users.noreply.github.com>
2020-10-24 22:36:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById('menu');
+const to_top_btn = document.getElementById('to_top_btn');
 
 menu.onclick = function MenuFunction() {
     var x = document.getElementById("myTopnav");
@@ -7,4 +8,25 @@ menu.onclick = function MenuFunction() {
     }else {
         x.className = 'topnav';
     }
+}
+
+window.onscroll = () => {
+    scrollFunction();
+}
+
+to_top_btn.onclick = function toTop() {
+    topFunction();
+}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("to_top_box").style.display = "block";
+    }else {
+        document.getElementById("to_top_box").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
